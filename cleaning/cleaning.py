599cc8936cdb9c2adb6977bd800af10678cef44f
JackCT1 <jackthomas933@gmail.com>
2024-03-04 16:36:07 +0000

    handler function

--- cleaning/cleaning.py
+++ cleaning/cleaning.py
@@ -79,3 +79,21 @@ def get_correct_timings(staging_df:pd.DataFrame) -> pd.DataFrame:
         staging_df = staging_df[timing_filter]
         
         return staging_df
+
+def handler(event,context):
+        """
+        Handler for Lambda function
+        """
+        print('Writing production table...')
+        staging_df = snake_case_column_names(staging_df)
+        staging_df = format_column_postcode(staging_df)
+        staging_df = remove_negative_age(staging_df)
+        staging_df = format_is_first(staging_df)
+        staging_df = get_positive_order_quantities(staging_df)
+        staging_df = change_columns_to_datetime(staging_df)
+        production_df = get_correct_timings(staging_df)
+        sql.write_df_to_table(
+                production_df,
+                schema_name='week4_jack_production',
+                table_name='production_ecommerce')
+        print('Success, production table written to database')
