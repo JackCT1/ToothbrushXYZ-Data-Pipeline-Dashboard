6349e982b53eb9ea09c2e79d0f86435ba9bd7359
JackCT1 <jackthomas933@gmail.com>
2024-03-05 15:44:06 +0000

    added app layout

--- web-app/app.py
+++ web-app/app.py
@@ -1,2 +1,21 @@
 from dash import Dash, dcc, html, Input, Output, page_container, page_registry
 import dash_bootstrap_components as dbc
+
+app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.VAPOR])
+
+app.layout = html.Div([
+    html.Div(
+        [
+            html.Div(
+                dcc.Link(
+                    f"{page['name']} - {page['path']}", href=page["relative_path"]
+                )
+            )
+            for page in page_registry.values()
+        ]
+    ),
+    page_container
+])
+
+if __name__ == "__main__":
+    app.run_server(host="0.0.0.0", debug=True, port=8080)
